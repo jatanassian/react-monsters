3903c7821d974d57746d94c89c8232e23f0ea843
Julien Atanassian <julien.atanassian@gmail.com>
2022-10-03 11:59:39 -0400

    implement card-list component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
 import "./App.css";
 
 class App extends Component {
@@ -48,13 +49,7 @@ class App extends Component {
           onChange={onSearchChange}
         />
 
-        {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.id}>
-              <h1>{monster.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
